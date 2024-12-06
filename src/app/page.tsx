b89a5a8ebae5253c476616252a5ea5cd997bd695
jay <emma.jordan@bsu.edu>
2024-12-06 11:54:33 -0500

    almost there -- meed to figure out how to update input 2 after button click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@ import { use, useState } from "react";
 
 export default function Home() {
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState('40');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(event.target.value)
+  }
+  
   return (
   <div className="grid grid-rows-[20px_1fr_20px] items-center justify">
-    <h1>{count}</h1>
 
-    <input type="text" value={setCount}></input>
-    <button type="submit" onClick={() => {setCount}}>Click Me!</button>
+    <input type="text" value={count} onChange={handleChange}></input>
+    <button type="submit" value="submit">Click Me!</button>
 
     <input type="text" value={count} readOnly></input>
   </div>
